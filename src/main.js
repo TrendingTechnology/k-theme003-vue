431e67871eceb4ec5893bbd1c7c69160fdd50d29
kdev <kdev.software1110@gmail.com>
2019-11-07 12:08:41 -0500

    vuesax integration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+
+import Vuesax from 'vuesax';
+import 'material-icons/iconfont/material-icons.css';
+import 'vuesax/dist/vuesax.css';
+Vue.use(Vuesax);
+
 import router from './router';
 
 Vue.config.productionTip = false;
