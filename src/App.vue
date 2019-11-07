07ffe6bd5533ce0a4d33447a49446e0f6a04c3db
kdev <kdev.software1110@gmail.com>
2019-11-07 06:33:29 -0500

    checking the routing for dashboard/analytics page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
