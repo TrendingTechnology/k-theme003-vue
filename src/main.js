e7cb03714eb4fe68a244c39e54f720e161c81473
kdev <kdev.software1110@gmail.com>
2019-11-07 13:22:07 -0500

    i18n integration

--- src/main.js
+++ src/main.js
@@ -15,11 +15,13 @@ import './config/theme.js';
 
 import router from './router';
 import store from './store';
+import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app');
