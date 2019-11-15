7ebfc4d82d3f47c614e9e8bcd1fcacb3c4a9d09c
kdev <kdev.software1110@gmail.com>
2019-11-15 11:01:44 -0500

    add route for ui-element/data-list/list-view

--- src/router/index.js
+++ src/router/index.js
@@ -1,124 +1,141 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import Vue from "vue";
+import Router from "vue-router";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-import auth from '@/service/auth';
+import auth from "@/service/auth";
 
 Vue.use(Router);
 
 const router = new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '',
-      component: () => import('@/layouts/main/Main.vue'),
+      path: "",
+      component: () => import("@/layouts/main/Main.vue"),
       children: [
         {
-          path: '/',
-          redirect: '/dashboard/analytics'
+          path: "/",
+          redirect: "/dashboard/analytics"
         },
         {
-          path: '/dashboard/analytics',
-          name: 'dashboard-analytics',
-          component: () => import('@/views/dashboard/Analytics.vue'),
+          path: "/dashboard/analytics",
+          name: "dashboard-analytics",
+          component: () => import("@/views/dashboard/Analytics.vue"),
           meta: {
-            rule: 'editor'
+            rule: "editor"
           }
         },
         {
-          path: '/dashboard/ecommerce',
-          name: 'dashboard-ecommerce',
-          component: () => import('@/views/dashboard/ECommerce.vue'),
+          path: "/dashboard/ecommerce",
+          name: "dashboard-ecommerce",
+          component: () => import("@/views/dashboard/ECommerce.vue"),
           meta: {
-            rule: 'admin'
+            rule: "admin"
           }
         },
         {
-          path: '/apps/email',
-          redirect: '/apps/email/inbox'
+          path: "/apps/email",
+          redirect: "/apps/email/inbox"
         },
         {
-          path: '/apps/email/:filter',
-          name: 'email',
-          component: () => import('@/views/apps/email/Email.vue'),
+          path: "/apps/email/:filter",
+          name: "email",
+          component: () => import("@/views/apps/email/Email.vue"),
           meta: {
-            rule: 'editor',
-            parent: 'email'
+            rule: "editor",
+            parent: "email"
           }
         },
         {
-          path: '/apps/chat',
-          name: 'chat',
-          component: () => import('@/views/apps/chat/Chat.vue'),
+          path: "/apps/chat",
+          name: "chat",
+          component: () => import("@/views/apps/chat/Chat.vue"),
           meta: {
-            rule: 'editor'
+            rule: "editor"
           }
         },
         {
-          path: '/apps/todo',
-          redirect: '/apps/todo/all'
+          path: "/apps/todo",
+          redirect: "/apps/todo/all"
         },
         {
-          path: '/apps/todo/:filter',
-          name: 'todo',
-          component: () => import('@/views/apps/todo/Todo.vue'),
+          path: "/apps/todo/:filter",
+          name: "todo",
+          component: () => import("@/views/apps/todo/Todo.vue"),
           meta: {
-            rule: 'editor',
-            parent: 'todo'
+            rule: "editor",
+            parent: "todo"
           }
         },
         {
-          path: '/apps/calendar/vue-simple-calendar',
-          name: 'calendar-simple-calendar',
-          component: () => import('@/views/apps/calendar/SimpleCalendar.vue'),
+          path: "/apps/calendar/vue-simple-calendar",
+          name: "calendar-simple-calendar",
+          component: () => import("@/views/apps/calendar/SimpleCalendar.vue"),
           meta: {
-            rule: 'editor'
+            rule: "editor"
           }
         },
         {
-          path: '/apps/eCommerce/shop',
-          name: 'ecommerce-shop',
-          component: () => import('@/views/apps/eCommerce/ECommerceShop.vue'),
+          path: "/apps/eCommerce/shop",
+          name: "ecommerce-shop",
+          component: () => import("@/views/apps/eCommerce/ECommerceShop.vue"),
           meta: {
             breadcrumb: [
-              { title: 'Home', url: '/' },
-              { title: 'eCommerce' },
-              { title: 'Shop', active: true }
+              { title: "Home", url: "/" },
+              { title: "eCommerce" },
+              { title: "Shop", active: true }
             ],
-            pageTitle: 'Shop',
-            rule: 'editor'
+            pageTitle: "Shop",
+            rule: "editor"
           }
         },
         {
-          path: '/apps/eCommerce/wish-list',
-          name: 'ecommerce-wish-list',
+          path: "/apps/eCommerce/wish-list",
+          name: "ecommerce-wish-list",
           component: () =>
-            import('@/views/apps/eCommerce/ECommerceWishList.vue'),
+            import("@/views/apps/eCommerce/ECommerceWishList.vue"),
           meta: {
             breadcrumb: [
-              { title: 'Home', url: '/' },
-              { title: 'eCommerce', url: '/apps/eCommerce/shop' },
-              { title: 'Wish List', active: true }
+              { title: "Home", url: "/" },
+              { title: "eCommerce", url: "/apps/eCommerce/shop" },
+              { title: "Wish List", active: true }
             ],
-            pageTitle: 'Wish List',
-            rule: 'editor'
+            pageTitle: "Wish List",
+            rule: "editor"
           }
         },
         {
-          path: '/apps/eCommerce/checkout',
-          name: 'ecommerce-checkout',
+          path: "/apps/eCommerce/checkout",
+          name: "ecommerce-checkout",
           component: () =>
-            import('@/views/apps/eCommerce/ECommerceCheckout.vue'),
+            import("@/views/apps/eCommerce/ECommerceCheckout.vue"),
           meta: {
             breadcrumb: [
-              { title: 'Home', url: '/' },
-              { title: 'eCommerce', url: '/apps/eCommerce/shop' },
-              { title: 'Checkout', active: true }
+              { title: "Home", url: "/" },
+              { title: "eCommerce", url: "/apps/eCommerce/shop" },
+              { title: "Checkout", active: true }
             ],
-            pageTitle: 'Checkout',
-            rule: 'editor'
+            pageTitle: "Checkout",
+            rule: "editor"
+          }
+        },
+        {
+          path: "/ui-elements/data-list/list-view",
+          name: "data-list-list-view",
+          component: () =>
+            import(
+              "@/views/ui-elements/data-list/list-view/DataListListView.vue"
+            ),
+          meta: {
+            breadcrumb: [
+              { title: "Home", url: "/" },
+              { title: "Data List" },
+              { title: "List View", active: true }
+            ],
+            pageTitle: "List View",
+            rule: "editor"
           }
         }
       ]
@@ -127,10 +144,10 @@ const router = new Router({
 });
 
 router.afterEach(() => {
-  const appLoading = document.getElementById('loading-bg');
+  const appLoading = document.getElementById("loading-bg");
 
   if (appLoading) {
-    appLoading.style.display = 'none';
+    appLoading.style.display = "none";
   }
 });
 
@@ -140,7 +157,7 @@ router.beforeEach((to, from, next) => {
 
     if (to.meta.authRequired) {
       if (!(auth.isAuthenticated() || firebaseCurrentUser)) {
-        router.push({ path: '/pages/login', query: { to: to.path } });
+        router.push({ path: "/pages/login", query: { to: to.path } });
       }
     }
 
