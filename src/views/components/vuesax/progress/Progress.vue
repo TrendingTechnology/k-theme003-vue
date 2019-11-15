e85ae3ceeeffe2d1506d4dd4257b3585fd7194f5
kdev <kdev.software1110@gmail.com>
2019-11-15 12:50:28 -0500

    implement progress components

---
+++ src/views/components/vuesax/progress/Progress.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="progress-demo">
+    <progress-color></progress-color>
+    <progress-interminate></progress-interminate>
+    <progress-height></progress-height>
+  </div>
+</template>
+
+<script>
+import ProgressColor from "./ProgressColor.vue";
+import ProgressInterminate from "./ProgressInterminate.vue";
+import ProgressHeight from "./ProgressHeight.vue";
+
+export default {
+  components: {
+    ProgressColor,
+    ProgressInterminate,
+    ProgressHeight
+  }
+};
+</script>
