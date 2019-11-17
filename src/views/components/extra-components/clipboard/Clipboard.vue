59ceea75c992a9a78730e56d53352ece3c29968d
kdev <kdev.software1110@gmail.com>
2019-11-17 12:32:43 -0500

    implement clipboard related components

---
+++ src/views/components/extra-components/clipboard/Clipboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="page-clipboard-demo">
+    <clipboard-directive></clipboard-directive>
+    <clipboard-without-directive></clipboard-without-directive>
+  </div>
+</template>
+
+<script>
+import ClipboardDirective from "./ClipboardDirective.vue";
+import ClipboardWithoutDirective from "./ClipboardWithoutDirective.vue";
+
+export default {
+  components: {
+    ClipboardDirective,
+    ClipboardWithoutDirective
+  }
+};
+</script>
